a82de2d7b57793f47a19e7ba5ad881333479bd4e
BigRichi <big.richi@outlook.com>
2021-05-01 00:45:14 -0400

    chart libiray installed

--- src/components/DashboardMain.js
+++ src/components/DashboardMain.js
@@ -1,8 +1,16 @@
+import { Box, Center, Button, Grid, Image, Text, GridItem} from "@chakra-ui/react"
 
-function DashboardMain() {
+function DashboardMain({allRockets, allLaunches, allLaunchpads}) {
 
     return(
-        <div>MAIN</div>
+        <Box>
+            <Box w="100%" h="500px" bgGradient="linear(to-t, blue.900, blue.200)" >
+                <div>MAIN 1</div>
+            </Box>
+            <Box w="100%" h="500px" bgGradient="linear(to-t, blue.200, blue.900)" >
+                <div>MAIN 2</div>
+            </Box>
+        </Box>
     )
 }
 
